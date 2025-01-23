9fc8e7c2fe9c14385f7b4aaf3a10286f946afbd6
Austin Mundy <austinmundy90@gmail.com>
2025-01-23 15:29:36 -0600

    main

--- backend/api/function_app.py
+++ backend/api/function_app.py
@@ -44,3 +44,46 @@ def GetResumeCounter(req: func.HttpRequest) -> func.HttpResponse:
             body="Error retrieving or updating data from Cosmos DB.",
             status_code=500
         )
+
+
+@app.route(route="GetResumeCounter1", auth_level=func.AuthLevel.FUNCTION)
+def GetResumeCounter1(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
+
+@app.route(route="GetResumeCounter2", auth_level=func.AuthLevel.FUNCTION)
+def GetResumeCounter2(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
